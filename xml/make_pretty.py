4dd67373b774d3b6b4aa85b19a6839de98dd3e54
davidjcmar <davidjcmar@gmail.com>
2015-10-17 12:33:35 -0500

    "remove sensitive info"

--- xml/make_pretty.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python -B
-# This program prints a pretty schema for any hotelml file
-from bs4 import BeautifulSoup as Soup
-import sys
-
-xml = open(sys.argv[1], "r")
-for line in xml:
-    soup = Soup(line, "xml")
-    print soup.prettify()
